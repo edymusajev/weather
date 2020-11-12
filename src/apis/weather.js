1bf119328fd907af770703431a5d15d59e7223f7
edehlol <edymusajev@gmail.com>
2020-11-12 18:44:19 +0200

    g

--- src/apis/weather.js
+++ src/apis/weather.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-export const getWeather = async () => {
-  const response = axios;
+const API_KEY = '1271256593c8dcf1f9d4048d228181a2';
+
+export const getWeather = async (lat, lon) => {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  );
+  return response;
 };
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getWeather } from '../apis/weather';
 
 const Weather = () => {
+  useEffect(() => {
+    getWeather(40, 44);
+  }, []);
+
   return (
     <div>
       <h4>Weather</h4>
