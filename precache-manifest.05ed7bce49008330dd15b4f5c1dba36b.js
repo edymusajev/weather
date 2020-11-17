d99bc50520f93df70f2f074df05f79ef5690c12d
edehlol <edymusajev@gmail.com>
2020-11-18 00:17:11 +0200

    Updates

---
+++ precache-manifest.05ed7bce49008330dd15b4f5c1dba36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2322b0794e21484fde3e8a1f7f22f20b",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "aa059734b8ee6e20fd78",
+    "url": "/weather/static/css/main.ef040e97.chunk.css"
+  },
+  {
+    "revision": "e7a813ec47d49b7a6e6c",
+    "url": "/weather/static/js/2.b52b6cd4.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/weather/static/js/2.b52b6cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa059734b8ee6e20fd78",
+    "url": "/weather/static/js/main.2b834bf5.chunk.js"
+  },
+  {
+    "revision": "ebac51f5882219bc7cf3",
+    "url": "/weather/static/js/runtime-main.1457f139.js"
+  }
+]);
